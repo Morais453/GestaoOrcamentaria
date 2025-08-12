cf2f265dc60d78d0ae1de98350373ff858e4bdab
Morais453 <danmorais453@hotmail.com>
2025-08-12 09:07:13 -0300

    Initialize project with Maven, Spring Boot, and basic application structure

---
+++ src/main/java/com/morais/Gestao/Orcamentaria/GestaoOrcamentariaApplication.java
@@ -0,0 +1,13 @@
+package com.morais.Gestao.Orcamentaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoOrcamentariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoOrcamentariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morais/Gestao/Orcamentaria/GestaoOrcamentariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morais.Gestao.Orcamentaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoOrcamentariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
